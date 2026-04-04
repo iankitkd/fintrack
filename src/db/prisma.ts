e2402bac0a4e43923aeab7c385dad2bcfcf2f508
Ankit Kumar <ankitkr.9105@gmail.com>
2026-04-04 19:06:19 +0530

    chore: init db

---
+++ src/db/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client.js";
+import { config } from "../config/index.js";
+
+const adapter = new PrismaPg({ connectionString: config.databaseUrl });
+
+export const prisma = new PrismaClient({
+  adapter,
+  log: ["query", "info", "warn", "error"],
+});
